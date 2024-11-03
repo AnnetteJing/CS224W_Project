a037dacca08dcfc794e2ccf26d49439b1df33dc0
Annette Jing <annette.jing@gmail.com>
2024-11-02 17:01:13 -0700

    Set up paths and update pyg-temporal

---
+++ __init__.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import git
+
+def get_git_root(path):
+    try:
+        repo = git.Repo(path, search_parent_directories=True)
+        return repo.working_tree_dir
+    except git.exc.InvalidGitRepositoryError:
+        return None
+
+ROOT_DIR = get_git_root(".")
+PYGT_PATH = os.path.join(ROOT_DIR, "pytorch_geometric_temporal")
+
+sys.path.extend([ROOT_DIR, PYGT_PATH])
